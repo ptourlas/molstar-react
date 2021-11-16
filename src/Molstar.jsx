5deb9c1c187388a000ed1cafba4c0bec95d4eca7
ptourlas <p.tourlas@acellera.com>
2021-11-16 15:00:40 +0200

    disable clear view

--- src/Molstar.jsx
+++ src/Molstar.jsx
@@ -5,15 +5,16 @@ import { Viewer } from "molstar/build/viewer/molstar";
 
 const Molstar = props => {
 
-  const { pdbId, url, dimensions, options } = props;
+  const { pdbId, url, dimensions, options, clearView } = props;
   const viewerElement = useRef(null);
   const viewer = useRef(null);
 
   useEffect(() => {
-    const mandatoryOptions = dimensions ? {layoutIsExpanded: false} : {};
-    const viewerOptions = {...(options || {}), ...mandatoryOptions};
+    const mandatoryOptions = dimensions ? { layoutIsExpanded: false } : {};
+    const viewerOptions = { ...(options || {}), ...mandatoryOptions };
     viewer.current = new Viewer(viewerElement.current, viewerOptions);
     if (pdbId) viewer.current.loadPdb(pdbId);
+    // if (clearView) viewer.current.plugin.state.data.dispose()
     if (url) viewer.current.loadStructureFromUrl(url);
     return () => viewer.current = null;
   }, [])
@@ -28,7 +29,7 @@ const Molstar = props => {
       <div ref={viewerElement} style={{
         position: "absolute",
         width: dimensions[0], height: dimensions[1], left: 0, right: 0
-      }}/>
+      }} />
     </div>
   );
 };
@@ -37,7 +38,8 @@ Molstar.propTypes = {
   pdbId: PropTypes.string,
   url: PropTypes.string,
   dimensions: PropTypes.array,
-  options: PropTypes.object
+  options: PropTypes.object,
+  clearView: PropTypes.bool
 };
 
 export default Molstar;
